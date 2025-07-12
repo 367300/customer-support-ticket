55583e0dfe93149c4e344987020007918969fd00
Вячеслав Литвиненко <l24122018@yandex.ru>
2025-07-12 21:27:36 +0300

    Добавил простой эндпоинт состояния сервиса

--- src/service/docker_build/service.py
+++ src/service/docker_build/service.py
@@ -81,6 +81,10 @@ def predict(query: Query):
         result = {'category': pred}
     return result
 
+@app.get('/health')
+def health():
+    return {"status": True}
+
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host='0.0.0.0', port=port) 
